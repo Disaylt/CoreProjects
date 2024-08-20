d8e72e73c48cdd87434bbd0257c3b0d3c2182eee
Disaylt <disofban@gmail.com>
2024-08-20 06:56:04 +0300

    init: create factory

---
+++ CoreProjects/Infrastructure/Configuration/ConfigurationFactory.cs
@@ -0,0 +1,23 @@
+﻿using CoreProjects.Application.Configuration;
+using CoreProjects.Domain.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProjects.Infrastructure.Configuration
+{
+    public class ConfigurationFactory
+    {
+        public virtual IConfigurationTemplate Create(ConfigurationTemplateType type, IConfigurationManager configurationManager)
+        {
+            return type switch
+            {
+                ConfigurationTemplateType.S3 => new S3ConfigurationTemplate(configurationManager),
+                _ => throw new Exception($"Type {type} is not registered.")
+            };
+        }
+    }
+}
