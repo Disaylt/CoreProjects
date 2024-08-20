f1d5eec2445bd8dd56ea5d3e82fd44a5f2232a31
Disaylt <disofban@gmail.com>
2024-08-20 06:30:55 +0300

    init: create template

---
+++ CoreProjects/Infrastructure/Configuration/S3ConfigurationTemplate.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.S3;
+using CoreProjects.Application.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProjects.Infrastructure.Configuration
+{
+    internal class S3ConfigurationTemplate : IConfigurationTemplate
+    {
+        protected IAmazonS3 Client { get; }
+        protected IConfigurationManager ConfigurationManager { get; }
+
+        public S3ConfigurationTemplate(IConfigurationManager configurationManager)
+        {
+            ConfigurationManager = configurationManager;
+        }
+
+        public Task LoadAsync(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
