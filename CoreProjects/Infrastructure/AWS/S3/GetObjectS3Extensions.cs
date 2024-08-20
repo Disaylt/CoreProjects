a126557a618dfcbd1672883cf52f30b677a5977c
Disaylt <disofban@gmail.com>
2024-08-20 05:44:40 +0300

    init: create s3 extension

---
+++ CoreProjects/Infrastructure/AWS/S3/GetObjectS3Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProjects.Infrastructure.AWS.S3
+{
+    public static class GetObjectS3Extensions
+    {
+        public static async Task<string> ReadAsStringAsync(this GetObjectResponse value)
+        {
+            using(StreamReader reader = new StreamReader(value.ResponseStream, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
