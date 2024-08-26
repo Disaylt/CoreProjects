3a58a2c02c134edcf51d1f8944e04136b2cd58d6
Disaylt <disofban@gmail.com>
2024-08-26 05:54:56 +0300

    feat: add new extension method

--- CoreProjects/Infrastructure/Configuration/Extensions.cs
+++ CoreProjects/Infrastructure/Configuration/Extensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using CoreProjects.Application.Configuration;
+using CoreProjects.Domain.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +12,8 @@ namespace CoreProjects.Infrastructure.Configuration
 {
     public static class Extensions
     {
+        private static readonly ConfigurationFactory _configurationFactory = new ConfigurationFactory();
+
         public static T GetValueRequired<T>(this IConfigurationManager configurationManager, string key)
         {
             return configurationManager.GetValue<T>(key)
@@ -19,6 +24,21 @@ namespace CoreProjects.Infrastructure.Configuration
         {
             return configurationManager.Get<T>()
                 ?? throw new NullReferenceException("Configuration not found.");
+        }
+
+        public static async Task AddApplicationConfiguration(this IConfigurationManager configuration, string applicationName, ConfigurationTemplateType type)
+        {
+            List<Task> tasks = new List<Task>();
+            IConfigurationTemplate configurationTemplate = _configurationFactory.Create(type, configuration);
+            IEnumerable<string> paths = configuration.GetValueRequired<IEnumerable<string>>(applicationName);
+
+            foreach (string path in paths)
+            {
+                Task task = configurationTemplate.LoadAsync(path);
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks);
         }
     }
 }
