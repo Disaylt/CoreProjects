5ee21e74a6bdf0065c32335bcf2b9b39008eabd0
Disaylt <disofban@gmail.com>
2024-08-26 05:30:13 +0300

    init: add configuration extensions

---
+++ CoreProjects/Domain/Common/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProjects.Domain.Common
+{
+    public static class Constants
+    {
+        private const string _configPathEnvName = "configPath";
+        public static string ConfigPathEnvName => _configPathEnvName;
+    }
+}
---
+++ CoreProjects/Infrastructure/Configuration/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProjects.Infrastructure.Configuration
+{
+    public static class Extensions
+    {
+        public static T GetValueRequired<T>(this IConfigurationManager configurationManager, string key)
+        {
+            return configurationManager.GetValue<T>(key)
+                ?? throw new NullReferenceException($"Key {key} not found");
+        }
+
+        public static T GetRequired<T>(this IConfigurationManager configurationManager)
+        {
+            return configurationManager.Get<T>()
+                ?? throw new NullReferenceException("Configuration not found.");
+        }
+    }
+}
